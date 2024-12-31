732a0e37588151a039d404c3d41b896de4704122
Aman Kumar <ak1223125@gmail.com>
2024-12-31 19:58:43 +0530

    new file

---
+++ basicsOfjava.java
@@ -0,0 +1,7 @@
+public class basicsOfjava{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("Upsc aspirant");
+        System.out.println(4+3);
+    }
+}
