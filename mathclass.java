fc5805657115bc2070e2a38b07d154279265f4c1
Aman Kumar <ak1223125@gmail.com>
2025-01-06 18:59:14 +0530

    math class

---
+++ mathclass.java
@@ -0,0 +1,20 @@
+public class mathclass {
+    public static void main(String [] args){
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2, 5);
+        result = Math.sqrt(81);
+        result = Math.abs(-6);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(5.1);
+        result = Math.max(12,34);
+        result = Math.min(12,8);
+
+        System.out.println(result);
+    }
+}
