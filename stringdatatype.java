6cffc161283cd1c6d4800ddd65a6078d018310eb
Aman Kumar <ak1223125@gmail.com>
2025-01-07 12:57:26 +0530

    String basics

---
+++ stringdatatype.java
@@ -0,0 +1,21 @@
+public class stringdatatype {
+    public static void main(String[] args){
+
+        String name = "bro";
+
+        int length = name.length();
+        char letter = name.charAt(1);
+        int index = name.indexOf("o");
+        int lastIndex = name.lastIndexOf("o");
+
+        //name = name.toUpperCase();
+        //name = name.toLowerCase();
+
+        //name = name.trim();
+        //name = name.replace("r","o");
+        
+
+
+        System.out.print(name);
+    }
+}
